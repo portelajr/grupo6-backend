18a2df01596ce6a373fb8bdf9f5872ece43651b1
Junior Portela <portelajr14@gmail.com>
2021-12-12 15:19:47 -0300

    userModel e userService + pasta utils com validçoes user e errorGenerator

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,15 @@
 const getConnection = require('./connection')
 
-// const createUser= async (nome, senha, email) => {
-//   const db = await getConnection();
+const createUser= async (name, password, email) => {
+  const db = await getConnection();
+  
+  const user = await db.collection('users')
+    .insertOne({ name, email, password })
+  
+  const { id: insetedId } = user;
 
-// }
+  return { id, name, email };
+};
+
+module.exports = { createUser };
+
